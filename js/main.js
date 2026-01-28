5f5e9962772080bfb9e8c59d23a34c4153c65ccd
devanshu75 <devanshurana75@gmail.com>
2026-01-28 19:14:22 +0530

    menu changes done

--- js/main.js
+++ js/main.js
@@ -5,18 +5,3 @@ toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
   overlay.classList.toggle("active");
 });
-
-const spotlight = document.querySelector(".cursor-spotlight");
-
-menuOverlay.addEventListener("mousemove", (e) => {
-  spotlight.style.left = e.clientX + "px";
-  spotlight.style.top = e.clientY + "px";
-});
-
-menuOverlay.addEventListener("mouseenter", () => {
-  spotlight.style.opacity = 1;
-});
-
-menuOverlay.addEventListener("mouseleave", () => {
-  spotlight.style.opacity = 0;
-});
